63320691f6f77ceaed7cd03b98a84d21a032dd92
Jean Evans Pierre <pierevans@gmail.com>
2017-09-18 14:03:24 -0400

    Bootstrap on-network with go-swagger, ginkgo and glide (#1)
    * Bootstrap on-network with go-swagger and ginkgo
    
    Signed-off-by: Amanda Alvarez <amanda.alvarez@dell.com>
    
    * Added swagger api documentation
    
    Signed-off-by: Jean Pierre <jean.pierre@emc.com>
    
    * Added Dockerfile
    
    Signed-off-by: Jean Pierre <jean.pierre@emc.com>
    
    * Updated Dockerfile to get dependencies
    
    Signed-off-by: Jean Pierre <jean.pierre@emc.com>
    
    * Updated test run and docker build to use newly created Dockerfile
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

---
+++ controllers/about/about_test.go
@@ -0,0 +1,51 @@
+package about_test
+
+import (
+	. "github.com/RackHD/on-network/controllers/about"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io"
+	"encoding/json"
+	"net/http/httptest"
+	"net/http"
+	"github.com/RackHD/on-network/models"
+)
+
+// TestProducer is ...
+type TestProducer struct{}
+
+//Produce is ...
+func (t TestProducer) Produce(w io.Writer, data interface{}) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(data)
+}
+
+var _ = Describe("About", func() {
+	var prod TestProducer
+	var buff *httptest.ResponseRecorder
+	BeforeEach(func() {
+		// Set up receiver to mock out where response would go
+		prod = TestProducer{}
+		buff = httptest.NewRecorder()
+	})
+	Context("When a message is routed to the /api/about handler", func() {
+		It("INTEGRATION info API should return an About object containing name='on-network'", func() {
+			// Create on-network api about
+			serverURL := "http://localhost:8080"
+			req, err := http.NewRequest("GET", serverURL+"/api/about", nil)
+
+			Expect(err).ToNot(HaveOccurred())
+			// Put HTTP Request into router
+			responder := MiddleWare(req)
+			responder.WriteResponse(buff, prod)
+
+			Expect(buff.Code).To(Equal(http.StatusOK))
+			about := models.About{}
+			err = json.Unmarshal(buff.Body.Bytes(), &about)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(*about.Name).To(Equal("on-network"))
+		})
+	})
+})
---
+++ restapi/doc.go
@@ -0,0 +1,19 @@
+/*Package restapi on-network api
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /api
+    Version: 0.0.1
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
