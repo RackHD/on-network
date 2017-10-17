3ccba056e42e4124bacb3c58f5fa532bbdb450aa
Massarrah Tannous <massarrah.tannous@emc.com>
2017-10-17 11:30:00 -0400

    Install all on 3172 (#16)
    * Bootstrap on-network with go-swagger and ginkgo
    
    Signed-off-by: Amanda Alvarez <amanda.alvarez@dell.com>
    
    * Added make docker
    Signed-off-by: Jean Pierre <jean.pierre@emc.com>
    
    * Added generated code for client
    
    Signed-off-by: Massarrah Tannous <massarrah.tannous@emc.com>
    
    * Cleaned up Readme
    
    Signed-off-by: Jean Pierre <jean.pierre@emc.com>
    
    * WIP install all for 6.0
    
    Signed-off-by: Jim Boettcher <james.boettcher@dell.com>
    
    * Finished implementation for install all using 6.0 firmware
    
    Signed-off-by: Jim Boettcher <james.boettcher@dell.com>

--- switch_operations/switch.go
+++ switch_operations/switch.go
@@ -1,8 +1,10 @@
 package switch_operations
 
+import "github.com/RackHD/on-network/models"
+
 // ISwitch is an interface for switch
 type ISwitch interface {
-	Update(string, string) error
+	Update(string, []*models.FirmwareImage) error
 	GetConfig() (string, error)
 	GetFirmware()(string, error)
 	GetFullVersion()(map[string] interface{}, error)
